b4109bae715eaf2586e6bb096bc6a1cc5484eb72
Maria Arquer <Maria Arquer@DESKTOP-IDHKT9P>
2019-07-01 16:28:31 +0200

    finito

--- CopiaLocalFinal/deCode.js
+++ CopiaLocalFinal/deCode.js
@@ -2,14 +2,14 @@
  * 
  */
 
-function enCode(matriu1, matriu2) {
-	var inputMatrix = matriu1;
-	var encodeMatrix01 = matriu2;
+function deCode(matriuCodif, decodeMatrix01) {
+	var matriuCodif = matriu3;
+	var decodeMatrix01 = matriu4;
 	var n = 3;
-	var matriu3 = new Array(3);
+	var matriu5 = new Array(3);
 
 	for (var j = 0; j < 3; j++) {
-		matriu3[j] = [];
+		matriu5[j] = [];
 		for (var l = 0; l < 3; l++) {
 			matriu3[j][l] = 0;
 		}
@@ -19,12 +19,12 @@ function enCode(matriu1, matriu2) {
 		for (colMa2 = 0; colMa2 < n; colMa2++) {
 			for (i = 0; i < n; i++) {
 
-				matriu3[rowMa1][colMa2] += (matriu1[rowMa1][i] * matriu2[i][colMa2]);
+				matriu5[rowMa1][colMa2] += (matriu3[rowMa1][i] * matriu4[i][colMa2]);
 				console.log(matriu3);
 			}
 
 		}
 
 	}
-	return matriu3
+	return matriu5
 }
