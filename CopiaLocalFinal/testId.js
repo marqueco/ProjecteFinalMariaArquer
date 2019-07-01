38c356976ae8a6170e6e2b2601419431aecead74
marqueco <51993667+marqueco@users.noreply.github.com>
2019-07-01 22:22:06 +0200

    Update testId.js

--- CopiaLocalFinal/testId.js
+++ CopiaLocalFinal/testId.js
@@ -2,16 +2,19 @@
  * 
  */
 
+// Aquesta funció detecta qui és l'autor o autora del vector entrant.
+// Autora: Maria Arquer
+// Paràmetres: el valor 'estudiant' retorna amb el nom de l'alumne
 
-function testID () {
-	var vectorMessage = inputMessage;
-	var nomsParticipants = ["Marc Formador","Maria Arquer","Pepi Balmaña","Gerard Barbero","Lionel Cioffi","Carlos Fabian","Joan Gonzalez","Jose Juarez","Montse Monclus","Javier Narciandi","Nuria Perez","Guillermo Rincon","Javi Sastre","Roger Sobrino"];
-	if (vectorMessage.length != 10){
-		return -1;
-	}
-	var p = vectorMessage[9];
-	
-	return "L'autor d'aquesta matriu es " + nomsParticipants[p]
+
+function testID (id) {
+var llistaAlumnes = [ "Marc Formador", "Maria Arquer", "Ma Jose Balmana",
+			"Gerard Barbero", "Lionel Cioffi", "Carlos Fabian",
+			"Juan Gonzalez", "Jose A Juarez", "Montse Monclus",
+			"Javier Narciandi", "Nuria Perez", "Guillermo Rincon",
+			"Javi Sastre", "Roger Sobrino" ];
+	var estudiant = llistaAlumnes[id];
+	return estudiant;
 }
 
 
